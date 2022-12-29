f1716c4dab527a6066085f917dd47ec282e76175
David Taylor <david@taylorhq.com>
2022-12-29 12:31:14 +0000

    DEV: Introduce syntax_tree for ruby formatting (#16)

--- app/controllers/client_performance/report_controller.rb
+++ app/controllers/client_performance/report_controller.rb
@@ -3,56 +3,71 @@
 class ClientPerformance::ReportController < ApplicationController
   skip_before_action :verify_authenticity_token, :check_xhr, :preload_json
 
-  NUMERIC_FIELDS = [
-    "time_to_first_byte",
-    "discourse_booted",
-    "dom_content_loaded",
-    "first_contentful_paint",
-    "largest_contentful_paint",
+  NUMERIC_FIELDS = %w[
+    time_to_first_byte
+    discourse_booted
+    dom_content_loaded
+    first_contentful_paint
+    largest_contentful_paint
   ]
 
-  ASSET_FIELDS = [
-    "local",
-    "app_cdn",
-    "s3_cdn"
-  ]
+  ASSET_FIELDS = %w[local app_cdn s3_cdn]
 
   JSON_SCHEMA = {
-      "type" => "object",
-      "required" => ["path", "time_to_first_byte", "assets"],
-      "properties" => {
-        "path" => { "type" => "string" },
-        **NUMERIC_FIELDS.map { |f| [f, { "type" => "number" }] }.to_h,
-        "assets" => {
-          "type" => "object",
-          "properties" => {
-            **ASSET_FIELDS.map do |k|
+    "type" => "object",
+    "required" => %w[path time_to_first_byte assets],
+    "properties" => {
+      "path" => {
+        "type" => "string",
+      },
+      **NUMERIC_FIELDS.map { |f| [f, { "type" => "number" }] }.to_h,
+      "assets" => {
+        "type" => "object",
+        "properties" => {
+          **ASSET_FIELDS
+            .map do |k|
               [
                 k,
                 {
                   "type" => "object",
                   "properties" => {
-                    "success_count" => { "type" => "integer" },
-                    "fail_count" => { "type" => "integer" },
-                    "pending_count" => { "type" => "integer" },
-                    "max_duration" => { "type" => "number" },
-                    "domain" => { "type" => "string" }
+                    "success_count" => {
+                      "type" => "integer",
+                    },
+                    "fail_count" => {
+                      "type" => "integer",
+                    },
+                    "pending_count" => {
+                      "type" => "integer",
+                    },
+                    "max_duration" => {
+                      "type" => "number",
+                    },
+                    "domain" => {
+                      "type" => "string",
+                    },
                   },
-                  "additionalProperties" => false
-                }
+                  "additionalProperties" => false,
+                },
               ]
-            end.to_h
-          },
-          "additionalProperties" => false
-        }
+            end
+            .to_h,
+        },
+        "additionalProperties" => false,
       },
-      "additionalProperties" => false
+    },
+    "additionalProperties" => false,
   }
 
   LOGS_PER_10_SECONDS = 2
 
   def report
-    RateLimiter.new(nil, "client_performance_report_#{current_user&.id || request.client_ip}", LOGS_PER_10_SECONDS, 10).performed!
+    RateLimiter.new(
+      nil,
+      "client_performance_report_#{current_user&.id || request.client_ip}",
+      LOGS_PER_10_SECONDS,
+      10,
+    ).performed!
 
     begin
       reported_data = JSON.parse(params.require("data"))
@@ -72,31 +87,28 @@ class ClientPerformance::ReportController < ApplicationController
     path = data["path"]
     data["url"] = { "domain" => Discourse.current_hostname, "path" => path }
 
-    route = begin
-      Rails.application.routes.recognize_path(path)
-    rescue ActionController::RoutingError
-      nil
-    end
+    route =
+      begin
+        Rails.application.routes.recognize_path(path)
+      rescue ActionController::RoutingError
+        nil
+      end
 
     data["discourse"] = {}
 
-    if route
-      data["discourse"]["route"] = "#{route[:controller]}/#{route[:action]}"
-    end
+    data["discourse"]["route"] = "#{route[:controller]}/#{route[:action]}" if route
 
     if current_user
       data["discourse"]["user"] = {
         "name" => current_user.username,
-        "staff" => current_user.staff?
+        "staff" => current_user.staff?,
       }
     end
 
     data["user_agent"] = { "original" => request.user_agent }
     data["source"] = { "address" => request.remote_ip }
 
-    data["discourse"]["client_perf"] = {
-      "after_ttfb" => {}
-    }
+    data["discourse"]["client_perf"] = { "after_ttfb" => {} }
 
     ttfb = reported_data["time_to_first_byte"]
 
@@ -125,22 +137,27 @@ class ClientPerformance::ReportController < ApplicationController
   end
 
   def self.raw_log(message)
-    @@logger ||= begin
-      f = File.open "#{Rails.root}/log/client-performance.log", "a"
-      f.sync = true
-      Logger.new f
-    end
+    @@logger ||=
+      begin
+        f = File.open "#{Rails.root}/log/client-performance.log", "a"
+        f.sync = true
+        Logger.new f
+      end
 
     @@log_queue ||= Queue.new
 
     if !defined?(@@log_thread) || !@@log_thread.alive?
-      @@log_thread = Thread.new do
-        loop do
-          @@logger << @@log_queue.pop
-        rescue Exception => e
-          Discourse.warn_exception(e, message: "Exception encountered while logging client performance")
+      @@log_thread =
+        Thread.new do
+          loop do
+            @@logger << @@log_queue.pop
+          rescue Exception => e
+            Discourse.warn_exception(
+              e,
+              message: "Exception encountered while logging client performance",
+            )
+          end
         end
-      end
     end
 
     @@log_queue.push(message)
